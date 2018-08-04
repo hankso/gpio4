4a69b5abcf1ee51026810a3f4dc0221baf58d630
hank <3080863354@qq.com>
2018-08-04 21:37:00 +0800

    fileno(attr="value")

--- __init__.py
+++ __init__.py
@@ -5,9 +5,105 @@ Created on Tue Apr 3 16:36:26 2018
 
 @author: hank
 @page:   https://github.com/hankso
+
+
+This gpio module is implemented on the basis of Sysfs gpio interface.
+What is Sysfs?
+Sysfs is a gpio access that been exposed to user space by kernel.
+
+Paths in Sysfs
+--------------
+There are three kinds of entry in /sys/class/gpio:
+
+   -	Control interfaces used to get userspace control over GPIOs;
+
+   -	GPIOs themselves; and
+
+   -	GPIO controllers ("gpio_chip" instances).
+
+That's in addition to standard files including the "device" symlink.
+
+The control interfaces are write-only:
+
+    /sys/class/gpio/
+
+    	"export" ... Userspace may ask the kernel to export control of
+		a GPIO to userspace by writing its number to this file.
+
+		Example:  "echo 19 > export" will create a "gpio19" node
+		for GPIO #19, if that's not requested by kernel code.
+
+    	"unexport" ... Reverses the effect of exporting to userspace.
+
+		Example:  "echo 19 > unexport" will remove a "gpio19"
+		node exported using the "export" file.
+
+GPIO signals have paths like /sys/class/gpio/gpio42/ (for GPIO #42)
+and have the following read/write attributes:
+
+    /sys/class/gpio/gpioN/
+
+	"direction" ... reads as either "in" or "out".  This value may
+		normally be written.  Writing as "out" defaults to
+		initializing the value as low.  To ensure glitch free
+		operation, values "low" and "high" may be written to
+		configure the GPIO as an output with that initial value.
+
+		Note that this attribute *will not exist* if the kernel
+		doesn't support changing the direction of a GPIO, or
+		it was exported by kernel code that didn't explicitly
+		allow userspace to reconfigure this GPIO's direction.
+
+	"value" ... reads as either 0 (low) or 1 (high).  If the GPIO
+		is configured as an output, this value may be written;
+		any nonzero value is treated as high.
+
+		If the pin can be configured as interrupt-generating interrupt
+		and if it has been configured to generate interrupts (see the
+		description of "edge"), you can poll(2) on that file and
+		poll(2) will return whenever the interrupt was triggered. If
+		you use poll(2), set the events POLLPRI and POLLERR. If you
+		use select(2), set the file descriptor in exceptfds. After
+		poll(2) returns, either lseek(2) to the beginning of the sysfs
+		file and read the new value or close the file and re-open it
+		to read the value.
+
+	"edge" ... reads as either "none", "rising", "falling", or
+		"both". Write these strings to select the signal edge(s)
+		that will make poll(2) on the "value" file return.
+
+		This file exists only if the pin can be configured as an
+		interrupt generating input pin.
+
+	"active_low" ... reads as either 0 (false) or 1 (true).  Write
+		any nonzero value to invert the value attribute both
+		for reading and writing.  Existing and subsequent
+		poll(2) support configuration via the edge attribute
+		for "rising" and "falling" edges will follow this
+		setting.
+
+GPIO controllers have paths like /sys/class/gpio/gpiochip42/ (for the
+controller implementing GPIOs starting at #42) and have the following
+read-only attributes:
+
+    /sys/class/gpio/gpiochipN/
+
+    	"base" ... same as N, the first GPIO managed by this chip
+
+    	"label" ... provided for diagnostics (not always unique)
+
+    	"ngpio" ... how many GPIOs this manges (N to N + ngpio - 1)
+
+Board documentation should in most cases cover what GPIOs are used for
+what purposes.  However, those numbers are not always stable; GPIOs on
+a daughtercard might be different depending on the base board being used,
+or other cards in the stack.  In such cases, you may need to use the
+gpiochip nodes (possibly in conjunction with schematics) to determine
+the correct GPIO number to use for a given signal.
 """
 import os
 import threading
+import select
 from . import constants
 
 
@@ -79,26 +175,27 @@ class SysfsGPIO(object):
     def edge(self, data):
         self._write('edge', data)
 
+    def fileno(self, attr='value'):
+        return self._file[attr].fileno()
+
     def _read(self, attr):
-        self._read_lock.acquire()
-        self._file[attr].seek(0)
-        value = self._file[attr].read().strip()
-        self._read_lock.release()
+        with self._read_lock:
+            self._file[attr].seek(0)
+            value = self._file[attr].read().strip()
         return value
 
     def _write(self, attr, data):
-        self._write_lock.acquire()
-        self._file[attr].seek(0)
-        self._file[attr].write(str(data))
-        self._write_lock.release()
+        with self._write_lock:
+            self._file[attr].seek(0)
+            self._file[attr].write(str(data))
 
 
 class _GPIO(object):
     def __init__(self):
         self.IN = constants.INPUT
         self.OUT = constants.OUTPUT
-        self.PULLUP = constants.INPUT_PULLUP
-        self.PULLDN = constants.INPUT_PULLDN
+        # self.PULLUP = constants.INPUT_PULLUP
+        # self.PULLDN = constants.INPUT_PULLDN
         self.HIGH = constants.HIGH
         self.LOW = constants.LOW
         self.RISING = constants.RISING
@@ -108,12 +205,12 @@ class _GPIO(object):
         self.BCM = constants.BCM
         self.VERSION = 1.0
 
+        self._mode = self.BOARD  # default mode
         self._pin_dict = {}
         self._pwm_dict = {}
         self._irq_dict = {}
-        self._mode = self.BOARD
         self._flag_interrupts = threading.Event()
-        self.enable_interrupts()
+        self._epoll = select.epoll()
 
     def _time_ms(self):
         return time.time() * 1000
@@ -158,17 +255,17 @@ class _GPIO(object):
 
         # register all pins and init them
         for p, s, i in zip(pins, states, initials):
-            if s == self.PULLUP:
-                s, i = self.IN, self.HIGH
-            elif s == self.PULLDN:
-                s, i = self.IN, self.LOW
-            elif s not in [self.IN, self.OUT]:
+            # if s == self.PULLUP:
+            #     s, i = self.IN, self.HIGH
+            # elif s == self.PULLDN:
+            #     s, i = self.IN, self.LOW
+            if s not in [self.IN, self.OUT]:
                 raise ValueError('Invalid state: {}!'.format(s))
             if p not in self._pin_dict:
                 self._pin_dict[p] = SysfsGPIO(p)
                 self._pin_dict[p].export = True
             self._pin_dict[p].direction = s
-            if i in [self.HIGH, self.LOW]:
+            if s == self.OUT and i in [self.HIGH, self.LOW]:
                 self._pin_dict[p].value = i
 
     def input(self, pin):
@@ -206,75 +303,70 @@ class _GPIO(object):
             if irq:
                 irq['flag_stop'].set()
                 irq['flag_triggered'].clear()
-            del pin, pwm, irq
 
     def enable_interrupts(self):
-        self._flag_interrupts.set()
+        if hasattr(self, '_thread_irq'):
+            self._flag_interrupts_pause.clear()
+            return
+        self._thread_irq = threading.Thread(target=self._handle_interrupts)
+        self._thread_irq.setDeamon(True)
+        self._thread_irq.start()
 
     def disable_interrupts(self):
-        self._flag_interrupts.clear()
+        self._flag_interrupts_pause.set()
 
-    def _soft_interrupt(self, sysgpio, flag_stop, flag_triggered):
-        # in case user set pin to some mode like PULLUP, we must check
-        # whether this pin is initialized as IN first
-        # default PULLUP/PULLDN are not used
-        if sysfsgpio.direction != self.IN:
-            sysfsgpio.direction = self.IN
-        while not flag_stop.isSet():
-            l1, l2 = self._irq_dict[p]['l1'], self._irq_dict['l2']
-            bouncetime = self._irq_dict[p]['bouncetime'] / 1000.0
-            while sysfsgpio.value != l1:
-                self._flag_interrupts.wait()
-            while sysfsgpio.value != l2:
-                self._flag_interrupts.wait()
-            time.sleep(bouncetime)
-            if sysfsgpio.value == l2:
-                flag_triggered.set()
+    def close_interrupts(self):
+        self._flag_interrupts_stop.set()
+
+    def _handle_interrupts(self):
+        while not self._flag_interrupts_stop.isSet():
+            self._flag_interrupts_pause.wait()
+            rst = self._epoll.poll(timeout=1)
+            if not rst:
+                continue
+            for fd, event in rst:
+                for p in self._irq_dict:
+                    if self._irq_dict[p]['fd'] == fd:
+                        bouncetime = self._irq_dict[p]['bouncetime']
+                        break
+                if bouncetime:
+                    time.sleep(bouncetime / 1000.0)
+                    edge, value = self._pin_dict[p].edge, self._pin_dict[p].value
+                    if (edge == self.RISING and value != self.HIGH) \
+                    or (edge == self.FALLING and value != self.LOW):
+                        continue
+                self._irq_dict[p]['interrupted'].set()
                 for c in self._irq_dict[p]['callbacks']:
-                    c(pin)
+                    try:
+                        c(self._irq_dict[p]['pin_name'])
+                    except:
+                        pass
+        print('[GPIO interrupts handler] shutdown')
 
     def add_event_detect(self, pin, edge, func=None, bouncetime=None):
         p = self._get_pin_num(pin, must_in_dict=True)
-        if p in self._irq_dict:
-            raise NameError(('Pin {} is already been attached to a soft '
-                             'interrupt on {} edge, if you want to reset it, '
-                             'please run `GPIO.remove_event_detect({})`'
-                             '').format(pin, self._irq_dict[p]['edge'], pin))
-        if edge == self.RISING:
-            l1, l2 = self.LOW, self.HIGH
-        elif edge == self.FALLING:
-            l1, l2 = self.HIGH, self.LOW
-        elif edge == self.BOTH:
-            l1 = sysfsgpio.value
-            l2 = self.HIGH - l1
-        else:
+        if edge not in [self.RISING, self.FALLING, self.BOTH]:
             raise ValueError('Invalid edge: {}'.format(edge))
-
-        # initialize soft interrupt on this pin
-        flag_stop, flag_triggered = threading.Event(), threading.Event()
-        t = threading.Thread(target=self._soft_interrupt,
-                             args=(self._pin_dict[p],
-                                   flag_stop,
-                                   flag_triggered))
-        t.setDeamon(True)
-        t.start()
+        if p in self._irq_dict:
+            raise NameError(('Pin {} is already been attached to an interrupt '
+                             'on {} edge, if you want to reset it, please run '
+                             '`GPIO.remove_event_detect({})` first'
+                             '').format(pin, self._pin_dict[p].edge, pin))
+        self._pin_dict[p].direction = 'in'
+        self._pin_dict[p].edge = edge
+        fd = self._pin_dict[p].fileno('value')
         self._irq_dict[p] = {
+            'fd': fd, 'interrupted': threading.Event(), 'pin_name': pin,
             'bouncetime': bouncetime if bouncetime is not None else 0,
-            'callbacks': self._listify(func) if func is not None else [],
-            'edge': edge, 'l1': l1, 'l2': l2,
-            'flag_stop': flag_stop, 'flag_triggered': flag_triggered
+            'callbacks': self._listify(func) if func is not None else []
         }
+        self._epoll.register(fd, select.EPOLLPRI | select.EPOLLET)
 
     def remove_event_detect(self, pin):
         p = self._get_pin_num(pin, must_in_dict=True)
-        if p not in self._irq_dict:
-            raise NameError(('Pin {} is not used as interrupt source yet, '
-                             'please run `GPIO.add_event_detect({}, edge)` to '
-                             'attached a interrupt first').format(pin, pin))
-        irq = self._irq_dict.pop(p)
-        irq['flag_stop'].set()
-        irq['flag_triggered'].clear()
-        del irq
+        if p in self._irq_dict:
+            self._epoll.unregister(self._irq_dict[p]['fd'])
+            self._irq_dict.remove(p)
 
     def add_event_callback(self, pin, callback):
         p = self._get_pin_num(pin, must_in_dict=True)
@@ -284,38 +376,21 @@ class _GPIO(object):
                              '').format(pin, pin))
         self._irq_dict[p]['callbacks'] += self._listify(callback)
 
-    def event_detected(self, pin, timeout=constants.FOREVER_ms):
-        p = self._get_pin_num(pin, must_in_dict=True)
-        if p not in self._irq_dict:
-            raise NameError(('Pin {} is not initialized with edge yet, please '
-                             'run `GPIO.add_event_detect({}, edge)` first'
-                             '').format(pin, pin))
-        start = self._time_ms()
-        while not self._irq_dict[p]['flag_triggered'].isSet():
-            if (self._time_ms() - start) > timeout:
-                return False
-            time.sleep(1.0/10)  # sensibility: refresh 10 times per second
-        self._irq_dict[p]['flag_triggered'].clear()
-        return True
-
     def wait_for_edge(self, pin, edge, timeout=constants.FOREVER_ms):
-        start = self._time_ms()
         p = self._get_pin_num(pin, must_in_dict=True)
-        if edge == self.RISING:
-            l1, l2 = self.LOW, self.HIGH
-        elif edge == self.FALLING:
-            l1, l2 = self.HIGH, self.LOW
-        elif edge == self.BOTH:
-            l1 = self._pin_dict[p].value
-            l2 = self.HIGH - l1
-        else:
+        if edge not in [self.RISING, self.FALLING, self.BOTH]:
             raise ValueError('Invalid edge: {}'.format(edge))
-        while self._pin_dict[p].value != l1:
+        if p in self._irq_dict and edge != self._pin_dict[p].edge:
+            raise NameError(('Pin {} is already been attached to an interrupt '
+                             'on {} edge, if you want to reset it, please run '
+                             '`GPIO.remove_event_detect({})` first'
+                             '').format(pin, self._pin_dict[p].edge, pin))
+        start = self._time_ms()
+        while not self._irq_dict[p]['interrupted'].isSet():
             if (self._time_ms() - start) > timeout:
-                return
-        while self._pin_dict[p].value != l2:
-            if (self._time_ms() - start) > timeout:
-                return
+                return None
+            time.sleep(1.0/10)  # sensibility: refresh 10 times per second
+        self._irq_dict[p]['interrupted'].clear()
         return pin
 
     def setmode(self, m):
